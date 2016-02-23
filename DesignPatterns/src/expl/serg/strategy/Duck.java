556c02bead3f8de1e185754453aad45fd86f6597
smac <sin_s_n@mail.ru>
2016-02-23 13:19:11 +0300

    Edit Duck

--- DesignPatterns/src/expl/serg/strategy/Duck.java
+++ DesignPatterns/src/expl/serg/strategy/Duck.java
@@ -19,4 +19,12 @@ public abstract class Duck {
 	public void swim() {
 		System.out.println("All duck float, even decoys!");
 	}
+	
+	public void setQuackBehavior(QuackBehavior qb) {
+		quackBehavior = qb;
+	}
+	
+	public void setFlyBehavior(FlyBehavior fb) {
+		flyBehavior = fb;
+	}
 }
--- DesignPatterns/src/expl/serg/strategy/FlyWithWings.java
+++ DesignPatterns/src/expl/serg/strategy/FlyWithWings.java
@@ -4,7 +4,7 @@ public class FlyWithWings implements FlyBehavior {
 
 	@Override
 	public void fly() {
-		System.out.println("I'm flying");
+		System.out.println("I'm flying!");
 		
 	}
 
