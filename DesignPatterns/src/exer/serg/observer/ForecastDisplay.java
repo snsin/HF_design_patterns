901f95d87cab35c1d875cadb65d127cfbf36e8f2
smac <sin_s_n@mail.ru>
2016-02-23 19:15:56 +0300

    ForecastDisplay added

---
+++ DesignPatterns/src/exer/serg/observer/ForecastDisplay.java
@@ -0,0 +1,17 @@
+package exer.serg.observer;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
