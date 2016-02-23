c3fd10d484779407dd8f5e8e8d8bf0219a29e09d
smac <sin_s_n@mail.ru>
2016-02-23 21:18:11 +0300

    CurrentConditionsDisplay

--- DesignPatterns/src/exer/serg/observer/CurrentConditionsDisplay.java
+++ DesignPatterns/src/exer/serg/observer/CurrentConditionsDisplay.java
@@ -1,27 +1,45 @@
 package exer.serg.observer;
 
-public class CurrentConditionsDisplay implements ObserverOld, DisplayElement {
+import java.util.Observable;
+import java.util.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
 	private float temperature;
 	private float humidity;
 //	private float pressure;
-	private Subject weatherData;
+	private Observable weatherData;
 	
-	public CurrentConditionsDisplay(Subject weatherData) {
+	public CurrentConditionsDisplay(Observable weatherData) {
+		weatherData.addObserver(this);
 		this.weatherData = weatherData;
-		weatherData.registerObserver(this);
 	}
 
-	@Override
+/*	@Override
 	public void update(float temperature, float humidity, float pressure) {
 		this.temperature = temperature;
 		this.humidity = humidity;
 		display();
 	}
-
+*/
 	@Override
 	public void display() {
 		System.out.println("Current conditions: " + temperature + "F degrees and "
 				+ humidity + "% humidity");
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if (o instanceof WeatherData) {
+			WeatherData weatherData = (WeatherData) o;
+			this.temperature = weatherData.getTemperature();
+			this.humidity = weatherData.getHumidity();
+			display();
+		}
+		
+	}
+	
+	public void removeSubj() {
+		weatherData.deleteObserver(this);	
 	}
 
 }
