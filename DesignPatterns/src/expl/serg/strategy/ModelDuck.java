18009680f9df9522ade8f85cab7a991c8812ee2c
smac <sin_s_n@mail.ru>
2016-02-23 13:32:07 +0300

    Constructor added to ModelDuck

--- DesignPatterns/src/expl/serg/strategy/ModelDuck.java
+++ DesignPatterns/src/expl/serg/strategy/ModelDuck.java
@@ -1,6 +1,11 @@
 package expl.serg.strategy;
 
 public class ModelDuck extends Duck {
+	
+	public ModelDuck() {
+		flyBehavior = new FlyNoWay();
+		quackBehavior = new Quack();
+	}
 
 	@Override
 	public void display() {
