97eb2062726f238963a6909b183e692ada4942a5
smac <sin_s_n@mail.ru>
2016-02-23 19:00:12 +0300

    CurrentConditionsDisplay added

---
+++ DesignPatterns/src/exer/serg/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,18 @@
+package exer.serg.observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
