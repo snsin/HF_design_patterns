eb4abbed921b53338301fc6ab3a699cdede6f0e4
smac <sin_s_n@mail.ru>
2016-02-23 18:53:56 +0300

    WeaterData realized

--- DesignPatterns/src/exer/serg/observer/WeaterData.java
+++ DesignPatterns/src/exer/serg/observer/WeaterData.java
@@ -1,5 +1,6 @@
 package exer.serg.observer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WeaterData implements Subject {
@@ -7,22 +8,38 @@ public class WeaterData implements Subject {
 	private float temperature;
 	private float humidity;
 	private float pressure;
+	
+	public WeaterData() {
+		this.observers = new ArrayList<>();
+	}
 	@Override
 	public void registerObserver(Observer o) {
-		// TODO Auto-generated method stub
-
+		observers.add(o);
 	}
 
 	@Override
 	public void removeObserver(Observer o) {
-		// TODO Auto-generated method stub
+		if (observers.contains(o)) {
+			observers.remove(o);
+		}
 
 	}
 
 	@Override
 	public void notifyObservers() {
-		// TODO Auto-generated method stub
-
+		for (Observer observer : observers) {
+			observer.update(temperature, humidity, pressure);
+		}
+	}
+	
+	public void measurementsChanged() {
+		notifyObservers();
+	}
+	
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
 	}
 
 }
