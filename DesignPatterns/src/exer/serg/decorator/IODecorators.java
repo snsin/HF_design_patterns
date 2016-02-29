89223cab1f5e34f6ee95330c3c3bdd9fd9266e11
smac <sin_s_n@mail.ru>
2016-03-01 00:54:27 +0300

    realized

--- DesignPatterns/src/exer/serg/decorator/IODecorators.java
+++ DesignPatterns/src/exer/serg/decorator/IODecorators.java
@@ -1,5 +1,27 @@
 package exer.serg.decorator;
 
-public class IODecorators {
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class IODecorators extends FilterInputStream{
+
+	protected IODecorators(InputStream in) {
+		super(in);
+	}
+	
+	public int read() throws IOException {
+		int ch = super.read();
+		return (ch == -1 ? ch : Character.toLowerCase((char) ch));
+	}
+	
+
+	public int read(byte b[], int offset, int len) throws IOException {
+		int result = super.read(b, offset, len);
+		for (int i = offset; i < offset + result; i++) {
+			b[i] = (byte) Character.toLowerCase((char) b[i]);
+		}
+		return result;
+		
+	}
 }
