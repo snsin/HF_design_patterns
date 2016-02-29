7a23747d370464dc47c8743b7d11dc84e6bade89
smac <sin_s_n@mail.ru>
2016-02-29 23:18:32 +0300

    DarkRoast realized

---
+++ DesignPatterns/src/exer/serg/decorator/DarkRoast.java
@@ -0,0 +1,14 @@
+package exer.serg.decorator;
+
+public class DarkRoast extends Beverage {
+	
+	public DarkRoast() {
+		description = "Dark Roast Coffee";
+	}
+
+	@Override
+	public double cost() {
+		return 0.99;
+	}
+
+}
