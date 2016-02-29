f15bbb196c23da8202e29bf527df0e1aa947c1fe
smac <sin_s_n@mail.ru>
2016-02-29 23:30:32 +0300

    two decorators realized

---
+++ DesignPatterns/src/exer/serg/decorator/Soy.java
@@ -0,0 +1,22 @@
+package exer.serg.decorator;
+
+public class Soy extends ConditimentDecorator {
+	
+	Beverage beverage;
+	
+	public Soy(Beverage beverage) {
+		this.beverage = beverage;
+
+	}
+
+	@Override
+	public String getDescription() {
+		return beverage.getDescription() + ", Soy";
+	}
+
+	@Override
+	public double cost() {
+		return 0.15 + beverage.cost();
+	}
+
+}
