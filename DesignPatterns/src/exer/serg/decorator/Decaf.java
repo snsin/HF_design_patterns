61fb4db92fc133f28d9c28b98e639160c7dcaa32
smac <sin_s_n@mail.ru>
2016-02-29 23:21:03 +0300

    Decaf realized

---
+++ DesignPatterns/src/exer/serg/decorator/Decaf.java
@@ -0,0 +1,15 @@
+package exer.serg.decorator;
+
+public class Decaf extends Beverage {
+	
+	public Decaf() {
+		description = "Decaffeinated coffee";
+	}
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
