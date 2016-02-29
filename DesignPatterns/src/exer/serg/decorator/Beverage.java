72c89b2f19e4ff01cf78ba2161cae136bda47935
smac <sin_s_n@mail.ru>
2016-03-01 00:08:09 +0300

    Ch

--- DesignPatterns/src/exer/serg/decorator/Beverage.java
+++ DesignPatterns/src/exer/serg/decorator/Beverage.java
@@ -1,11 +1,23 @@
 package exer.serg.decorator;
 
+
+
 public abstract class Beverage {
+	public enum Size {TALL, GRANDE, VENTY};
 	String description = "Unknown Beverage";
+	Size size;
 	public String getDescription() {
 		return description;
 	}
 	
 	public abstract double cost();
+	
+	public void setSize(Size size) {
+		this.size = size;
+	}
+	
+	public Size getSize() {
+		return size;
+	}
 
 }
