b50f7acda682de48f4147b0089552d2ea3e6b628
smac <sin_s_n@mail.ru>
2016-03-05 15:58:13 +0300

    realized PizzaStore

--- DesignPatterns/src/exer/serg/factory/PizzaStore.java
+++ DesignPatterns/src/exer/serg/factory/PizzaStore.java
@@ -2,6 +2,15 @@ package exer.serg.factory;
 
 public abstract class PizzaStore {
 	
+	public Pizza orderPizza(String type) {
+		Pizza pizza;
+		pizza = createPizza(type);
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+		return pizza;
+	}
 	
 	abstract Pizza createPizza(String type);
 }
