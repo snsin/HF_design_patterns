d66f66b271fe4e92d4081cffab0ef6ca1081e919
smac <sin_s_n@mail.ru>
2016-03-05 15:44:37 +0300

    Created

---
+++ DesignPatterns/src/exer/serg/factory/Pizza.java
@@ -0,0 +1,12 @@
+package exer.serg.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Pizza {
+	String name;
+	String dough;
+	String sauce;
+	List<String> topings = new ArrayList<>();
+
+}
---
+++ DesignPatterns/src/exer/serg/factory/PizzaStore.java
@@ -0,0 +1,7 @@
+package exer.serg.factory;
+
+public abstract class PizzaStore {
+	
+	
+	abstract Pizza createPizza(String type);
+}
