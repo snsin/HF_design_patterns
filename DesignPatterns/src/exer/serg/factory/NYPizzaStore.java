439a7e2f53d1f900048f7d8799d814428ae4b0c4
smac <sin_s_n@mail.ru>
2016-03-05 20:40:18 +0300

    ch

--- DesignPatterns/src/exer/serg/factory/NYPizzaStore.java
+++ DesignPatterns/src/exer/serg/factory/NYPizzaStore.java
@@ -5,8 +5,17 @@ public class NYPizzaStore extends PizzaStore {
 	@Override
 	Pizza createPizza(String type) {
 		Pizza result = null;
-		if (type.equals("ceese")) {
-			result = new NYStyleChesePizza();
+		if (type.equals("cheese")) {
+			result = new NYStyleCheesePizza();
+		}
+		if (type.equals("veggie")) {
+			result = new NYStyleVeggiePizza();
+		}
+		if (type.equals("clam")) {
+			result = new NYStyleClamPizza();
+		}
+		if (type.equals("pepperoni")) {
+			result = new NYStylePepperoniPizza();
 		}
 		return result;
 	}
