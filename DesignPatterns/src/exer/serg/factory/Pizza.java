7163f20edb4a16c153a651869df220e68f6e6082
smac <sin_s_n@mail.ru>
2016-03-05 15:54:59 +0300

    realized Pizza

--- DesignPatterns/src/exer/serg/factory/Pizza.java
+++ DesignPatterns/src/exer/serg/factory/Pizza.java
@@ -7,6 +7,33 @@ public abstract class Pizza {
 	String name;
 	String dough;
 	String sauce;
-	List<String> topings = new ArrayList<>();
+	List<String> toppings = new ArrayList<>();
+	
+	void prepare() {
+		System.out.println("Preparing " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings: ");
+		for (String topping : toppings) {
+			System.out.println("\t" + topping);
+		}
+	}
+	
+	void bake() {
+		System.out.println("Bake for 25 minutes at 350");
+	}
+	
+	void cut() {
+		System.out.println("Cutting the pizza into diagonal slices");
+	}
+	
+	void box() {
+		System.out.println("Place pizza in official PizzaStore box");
+	}
 
+	public String getName() {
+		return name;
+	}
+	
+	
 }
