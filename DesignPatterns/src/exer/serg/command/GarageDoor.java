576fe4784470d785e94d2cc1cc11ef5a4507f989
smac <sin_s_n@mail.ru>
2016-04-03 18:36:46 +0300

    GarageDoor v0.1

---
+++ DesignPatterns/src/exer/serg/command/GarageDoor.java
@@ -0,0 +1,24 @@
+package exer.serg.command;
+
+public class GarageDoor {
+	
+	public void up() {
+		System.out.println("Door goes up!");
+	}
+	
+	public void down() {
+		System.out.println("Door goes down!");
+	}
+	
+	public void stop() {
+		System.out.println("Door is stopped!");
+	}
+	
+	public void lightOn() {
+		System.out.println("Garage door light is on!");
+	}
+	
+	public void lightOff() {
+		System.out.println("Garage door light is off!");
+	}
+}
