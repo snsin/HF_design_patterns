d1b521cd8fc2be6d089ffa8ccf72a29fb8dd5d56
smac <sin_s_n@mail.ru>
2016-04-03 23:41:04 +0300

    state added

--- DesignPatterns/src/exer/serg/command/CeillingFan.java
+++ DesignPatterns/src/exer/serg/command/CeillingFan.java
@@ -1,27 +1,38 @@
 package exer.serg.command;
 
 public class CeillingFan {
+	enum State {OFF, LOW, MEDIUM, HIGH};
 	
 	private String place;
+	private State state;
 	
 	public CeillingFan(String place) {
 		this.place = place;
+		state = State.OFF;
 	}
 	
 	public void high() {
+		state = State.HIGH;
 		System.out.println(place + " ceilling fan is on high!");
 	}
 	
 	public void medium() {
+		state = State.MEDIUM;
 		System.out.println(place + " ceilling fan is on medium!");
 	}
 	
 	public void low() {
+		state = State.LOW;
 		System.out.println(place + " ceilling fan is on low!");
 	}
 	
 	public void off() {
+		state = State.OFF;
 		System.out.println(place + "  ceilling fan is off!");
+	}
+	
+	public State getState() {
+		return this.state;
 	}
 
 }
