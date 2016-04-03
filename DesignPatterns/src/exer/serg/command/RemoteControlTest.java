a01551d91e54156b16e3d400588a176153ba6aa2
smac <sin_s_n@mail.ru>
2016-04-03 20:12:55 +0300

    new class tested

--- DesignPatterns/src/exer/serg/command/RemoteControlTest.java
+++ DesignPatterns/src/exer/serg/command/RemoteControlTest.java
@@ -5,8 +5,14 @@ public class RemoteControlTest {
 	public static void main(String[] args) {
 		SimpleRemoteControl remote = new SimpleRemoteControl();
 		Light lt = new Light();
+		GarageDoor door = new GarageDoor();
 		LightOnCommand ightOn = new LightOnCommand(lt);
+		GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(door);
+		
+		
 		remote.setCommand(ightOn);
+		remote.buttonWasPressed();
+		remote.setCommand(garageOpen);
 		remote.buttonWasPressed();
 
 	}
