81d5bbac57fd75cb6e2d8393ba15f8a53b586a1a
smac <sin_s_n@mail.ru>
2016-04-03 21:16:12 +0300

    StereoOnWithCDCCommand is realized

---
+++ DesignPatterns/src/exer/serg/command/StereoOnWithCDCCommand.java
@@ -0,0 +1,18 @@
+package exer.serg.command;
+
+public class StereoOnWithCDCCommand implements Command {
+	
+	Stereo stereo;
+	
+	public StereoOnWithCDCCommand(Stereo stereo) {
+		this.stereo = stereo;
+	}
+
+	@Override
+	public void execute() {
+		stereo.on();
+		stereo.setCd();
+		stereo.setVolume(Stereo.DEFAULT_VOLUME_LEVEL);
+	}
+
+}
