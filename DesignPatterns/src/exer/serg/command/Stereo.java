2f41f5661d23ae1f6419d6913de0b72d028ff84f
smac <sin_s_n@mail.ru>
2016-04-03 21:31:38 +0300

    Stereo v0.1

--- DesignPatterns/src/exer/serg/command/Stereo.java
+++ DesignPatterns/src/exer/serg/command/Stereo.java
@@ -3,28 +3,33 @@ package exer.serg.command;
 public class Stereo {
 	
 	public static final int DEFAULT_VOLUME_LEVEL = 11;
+	private String place;
+	
+	public Stereo(String place) {
+		this.place = place;
+	}
 	
 	public void on() {
-		System.out.println("Stereo is on!");
+		System.out.println(place +  "stereo is on!");
 	}
 
 	public void off() {
-		System.out.println("Stereo is off!");
+		System.out.println(place +  "stereo is off!");
 	}
 	
 	public void setCd() {
-		System.out.println("CD is setted!");
+		System.out.println(place +  "stereo is set for CD input!");
 	}
 	
 	public void setDvd() {
-		System.out.println("DVD is setted!");
+		System.out.println(place +  "stereo is set for DVD input!");
 	}
 	
 	public void setRadio() {
-		System.out.println("Radio is setted!");
+		System.out.println(place +  "stereo is set for radio input!");
 	}
 	
 	public void setVolume(int level) {
-		System.out.println("Volume level is setted to " + level + "!");
+		System.out.println(place +  "stereo volume is setted to " + level + "!");
 	}
 }
