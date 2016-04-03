88f9ba6888ba3c9dae8a2b4540eb3df941d45d15
smac <sin_s_n@mail.ru>
2016-04-03 21:43:43 +0300

    CeillingFanOnCommand realized

---
+++ DesignPatterns/src/exer/serg/command/CeillingFanOnCommand.java
@@ -0,0 +1,15 @@
+package exer.serg.command;
+
+public class CeillingFanOnCommand implements Command {
+	CeillingFan fan;
+	
+	public CeillingFanOnCommand(CeillingFan fan) {
+		this.fan = fan;
+	}	
+
+	@Override
+	public void execute() {
+		fan.high();
+	}
+
+}
