66ad1a3fe84978f0c0e000ed9c6328a4adb6fb8d
smac <sin_s_n@mail.ru>
2016-04-03 22:20:17 +0300

    ttt

--- DesignPatterns/src/exer/serg/command/RemoteControlTest.java
+++ DesignPatterns/src/exer/serg/command/RemoteControlTest.java
@@ -3,18 +3,44 @@ package exer.serg.command;
 public class RemoteControlTest {
 
 	public static void main(String[] args) {
-		SimpleRemoteControl remote = new SimpleRemoteControl();
-		Light lt = new Light();
+		RemoteControl remote = new RemoteControl();
+		
+		Light livingRoomLight = new Light("Living Room");
+		Light kitchenLight = new Light("Kitchen");
+		CeillingFan ceillingFan = new CeillingFan("Living Room");
 		GarageDoor door = new GarageDoor();
-		LightOnCommand ightOn = new LightOnCommand(lt);
-		GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(door);
+		Stereo stereo = new Stereo("Living Room");
 		
+		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+		LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+		LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
 		
-		remote.setCommand(ightOn);
-		remote.buttonWasPressed();
-		remote.setCommand(garageOpen);
-		remote.buttonWasPressed();
-
+		CeillingFanOffCommand ceillingFanOff = new CeillingFanOffCommand(ceillingFan);
+		CeillingFanOnCommand ceillingFanOnCommand = new CeillingFanOnCommand(ceillingFan);
+		
+		GarageDoorUpCommand garageDoorUpCommand = new GarageDoorUpCommand(door);
+		GarageDoorDownCommand garageDoorDownCommand = new GarageDoorDownCommand(door);
+		
+		StereoOnWithCDCCommand stereoOnWithCDCCommand = new StereoOnWithCDCCommand(stereo);
+		StereoOffCommand stereoOffCommand = new StereoOffCommand(stereo);
+		
+		remote.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+		remote.setCommand(1, kitchenLightOn, kitchenLightOff);
+		remote.setCommand(2, ceillingFanOnCommand, ceillingFanOff);
+		remote.setCommand(3, stereoOnWithCDCCommand, stereoOffCommand);
+		remote.setCommand(4, garageDoorUpCommand, garageDoorDownCommand);
+		
+		System.out.println(remote);
+		
+		remote.onButtonWasPressed(0);
+		remote.offButtonWasPressed(0);
+		remote.onButtonWasPressed(1);
+		remote.offButtonWasPressed(1);
+		remote.onButtonWasPressed(2);
+		remote.offButtonWasPressed(2);
+		remote.onButtonWasPressed(3);
+		remote.offButtonWasPressed(3);
 	}
 
 }
