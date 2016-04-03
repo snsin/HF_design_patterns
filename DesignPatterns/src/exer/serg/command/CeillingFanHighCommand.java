b4a7e2689e6185a2390de9f9007c995598c6f113
smac <sin_s_n@mail.ru>
2016-04-03 23:44:54 +0300

    undo

--- DesignPatterns/src/exer/serg/command/CeillingFanHighCommand.java
+++ DesignPatterns/src/exer/serg/command/CeillingFanHighCommand.java
@@ -2,6 +2,7 @@ package exer.serg.command;
 
 public class CeillingFanHighCommand implements Command {
 	CeillingFan fan;
+	CeillingFan.State prevState;
 	
 	public CeillingFanHighCommand(CeillingFan fan) {
 		this.fan = fan;
@@ -9,12 +10,25 @@ public class CeillingFanHighCommand implements Command {
 
 	@Override
 	public void execute() {
+		prevState = fan.getState();
 		fan.high();
 	}
 
 	@Override
 	public void undo() {
-		fan.off();
+		switch (prevState) {
+		case OFF:
+			fan.off();
+			break;
+		case LOW:
+			fan.low();
+			break;
+		case MEDIUM:
+			fan.medium();
+			break;
+		case HIGH:
+			fan.high();
+			break;
+		}
 	}
-
 }
