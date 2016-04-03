33988113c14c379b6759c94b2503d018945e1e32
smac <sin_s_n@mail.ru>
2016-04-03 18:11:28 +0300

    Light realised

--- DesignPatterns/src/exer/serg/command/Light.java
+++ DesignPatterns/src/exer/serg/command/Light.java
@@ -1,5 +1,11 @@
 package exer.serg.command;
 
 public class Light {
+	public void on() {
+		System.out.println("Light is on!");
+	}
+	public void off() {
+		System.out.println("Light is off!");
+	}
 
 }
