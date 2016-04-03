32d79a337ad48640eae6cd47ca123d16e2505d58
smac <sin_s_n@mail.ru>
2016-04-04 00:40:41 +0300

    MacroCommand realized

--- DesignPatterns/src/exer/serg/command/MacroCommand.java
+++ DesignPatterns/src/exer/serg/command/MacroCommand.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MacroCommand implements Command {
-	ArrayList<Command> commands;
+	ArrayList<Command> commands = new ArrayList<>();
 	
 	public MacroCommand(Command[] commands) {
 		this.commands.addAll(Arrays.asList(commands));
