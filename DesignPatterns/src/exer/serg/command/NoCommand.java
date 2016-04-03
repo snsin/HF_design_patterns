f81b467023a5244197793d90b940ce4ab227cd96
smac <sin_s_n@mail.ru>
2016-04-03 22:58:25 +0300

    undo

--- DesignPatterns/src/exer/serg/command/NoCommand.java
+++ DesignPatterns/src/exer/serg/command/NoCommand.java
@@ -3,6 +3,13 @@ package exer.serg.command;
 public class NoCommand implements Command {
 
 	@Override
-	public void execute() {}
+	public void execute() {
+		System.out.println("Execute noCommand!");
+	}
+
+	@Override
+	public void undo() {
+		System.out.println("Undo noCommand!");
+	}
 
 }
