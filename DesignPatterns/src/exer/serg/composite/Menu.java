47c72d1069e93a8b9577b2627e62a12455463c7d
smac <sin_s_n@mail.ru>
2016-04-17 21:40:49 +0300

    Menu is realized

--- DesignPatterns/src/exer/serg/composite/Menu.java
+++ DesignPatterns/src/exer/serg/composite/Menu.java
@@ -1,6 +1,46 @@
 package exer.serg.composite;
 
+import java.util.ArrayList;
+
 public class Menu extends MenuComponent {
+	private ArrayList<MenuComponent> menuComponents = new ArrayList<>();
+	private String name;
+	private String description;
 	
+	public Menu(String name, String description) {
+		this.name = name;
+		this.description = description;		
+	}
+	
+	@Override
+	public void add(MenuComponent menuComponent) {
+		menuComponents.add(menuComponent);
+	}
+	
+	@Override
+	public void remove(MenuComponent menuComponent) {
+		menuComponents.remove(menuComponent);
+	}
+	
+	@Override
+	public MenuComponent getChild(int i) {
+		return menuComponents.get(i);
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String getDescription() {
+		return description;
+	}
+	
+	public void print() {
+		System.out.print("\n" + getName());
+		System.out.println(", " + getDescription());
+		System.out.println("----------------------");
+	}
 
 }
