505718ffa410731bad0f5cdb4215dd6b70a9886d
smac <sin_s_n@mail.ru>
2016-04-17 21:48:43 +0300

    Waitress is realized

--- DesignPatterns/src/exer/serg/composite/Waitress.java
+++ DesignPatterns/src/exer/serg/composite/Waitress.java
@@ -1,5 +1,14 @@
 package exer.serg.composite;
 
 public class Waitress {
+	MenuComponent allMenus;
+	
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+	
+	public void printMenu() {
+		allMenus.print();
+	}
 
 }
