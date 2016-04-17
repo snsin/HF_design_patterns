52603d10a1e14ff5d02cd79dbe267260beae4f87
smac <sin_s_n@mail.ru>
2016-04-17 14:55:14 +0300

    MenuItem is realized

--- DesignPatterns/src/exer/serg/composite/MenuItem.java
+++ DesignPatterns/src/exer/serg/composite/MenuItem.java
@@ -15,5 +15,35 @@ public class MenuItem extends MenuComponent {
 		this.vegeterian = vegeterian;
 		this.price = price;
 	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String getDescription() {
+		return description;
+	}
+	
+	@Override
+	public double getPrice() {
+		return price;
+	}
+	
+	@Override
+	public boolean isVegetarian() {
+		return vegeterian;
+	}
+	
+	@Override
+	public void print() {
+		System.out.print("   " + getName());
+		if (isVegetarian()) {
+			System.out.print("(v)");
+		}
+		System.out.println(", " + getPrice());
+		System.out.println("     -- " + getDescription());
+	}
 
 }
