40e9d9577e173b554f9971269b87b4b771a01b87
smac <sin_s_n@mail.ru>
2016-05-01 13:13:21 +0300

    SoldOutState realized

---
+++ DesignPatterns/src/exer/serg/state/SoldOutState.java
@@ -0,0 +1,36 @@
+package exer.serg.state;
+
+public class SoldOutState implements State {
+	
+	private GumballMachine gumballMachine;
+	
+	public SoldOutState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+		
+	}	
+
+	@Override
+	public void insertQuarter() {
+		System.out.println("You can't insert a coin. Machine is sold out!");
+
+	}
+
+	@Override
+	public void ejectQuarter() {
+		System.out.println("You didn't insert a quarter!");
+
+	}
+
+	@Override
+	public void turnCrunk() {
+		System.out.println("You turned but machine is sold out!");
+
+	}
+
+	@Override
+	public void dispens() {
+		System.out.println("No more gumballs!");
+
+	}
+
+}
