4a3f7b35f0dceea6f24961e5ced3749cfc18bcf3
smac <sin_s_n@mail.ru>
2016-05-01 14:24:56 +0300

    Test

---
+++ DesignPatterns/src/exer/serg/state/GumballMachineTestDrive.java
@@ -0,0 +1,23 @@
+package exer.serg.state;
+
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		GumballMachine gumballMachine = new GumballMachine(5);
+		System.out.println(gumballMachine);
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrunk();
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrunk();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrunk();
+		
+		System.out.println(gumballMachine);
+		
+
+	}
+
+}
