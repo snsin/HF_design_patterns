8765b5c308f1909703e249c15d7187e31f921b21
smac <sin_s_n@mail.ru>
2016-05-01 14:14:19 +0300

    HasQuarterState is modified

--- DesignPatterns/src/exer/serg/state/HasQuarterState.java
+++ DesignPatterns/src/exer/serg/state/HasQuarterState.java
@@ -1,7 +1,10 @@
 package exer.serg.state;
 
+import java.util.Random;
+
 public class HasQuarterState implements State {
 	GumballMachine gumballMachine;
+	Random randomWinner = new Random(System.currentTimeMillis());
 	 
 	public HasQuarterState(GumballMachine gumballMachine) {
 		this.gumballMachine = gumballMachine;
@@ -27,7 +30,13 @@ public class HasQuarterState implements State {
 
 	@Override
 	public void turnCrank() {
-		// TODO Auto-generated method stub
+		System.out.println("You turned...");
+		int winner = randomWinner.nextInt(10);
+		if (winner == 0 && gumballMachine.getCount() > 1) {
+			gumballMachine.setState(gumballMachine.getWinnerState());
+		} else {
+			gumballMachine.setState(gumballMachine.getSoldState());
+		}
 		
 	}
 
