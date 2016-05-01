6326fd1bea0e323d8f43f61ee8e3f9aaa42b021b
smac <sin_s_n@mail.ru>
2016-05-01 13:48:20 +0300

    WinnerState is realized

---
+++ DesignPatterns/src/exer/serg/state/WinnerState.java
@@ -0,0 +1,43 @@
+package exer.serg.state;
+
+public class WinnerState implements State {
+	private GumballMachine gumballMachine;
+	
+	public WinnerState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	@Override
+	public void insertQuarter() {
+		System.out.println("Please wait, we're already giving you a gumball");
+	}
+
+	@Override
+	public void ejectQuarter() {
+		System.out.println("Sorry, you already turned the crank");
+	}
+
+	@Override
+	public void turnCrunk() {
+		System.out.println("Turning twice doesn't get you another gumball!");
+	}
+
+	@Override
+	public void dispens() {
+		System.out.println("YOU'RE A WINNER! You get two gumballs for your quarter");
+		gumballMachine.releaseBall();
+		if (gumballMachine.getCount() == 0) {
+			gumballMachine.setState(gumballMachine.getSoldOutState());
+		} else {
+			gumballMachine.releaseBall();
+			if (gumballMachine.getCount() > 0) {
+				gumballMachine.setState(gumballMachine.getNoQuarterState());
+			} else {
+				System.out.println("Oops, out off gumballs!");
+				gumballMachine.setState(gumballMachine.getSoldOutState());
+			}
+		}
+
+	}
+
+}
