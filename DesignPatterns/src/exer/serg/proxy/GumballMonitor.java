84cd4bfdf01faca024d9a44c23044c24c597f6ad
smac <sin_s_n@mail.ru>
2016-05-15 22:27:44 +0300

    GumballMonitor first version is realized

--- DesignPatterns/src/exer/serg/proxy/GumballMonitor.java
+++ DesignPatterns/src/exer/serg/proxy/GumballMonitor.java
@@ -1,5 +1,21 @@
 package exer.serg.proxy;
 
+import exer.serg.state.GumballMachine;
+
 public class GumballMonitor {
+	GumballMachine machine;
+	
+	public GumballMonitor(GumballMachine machine) {
+		this.machine = machine;
+	}
+	
+	public void report() {
+		if (machine != null) {
+			System.out.println("Gumball Machine: " + machine.getLocation()); 
+			System.out.println("Current inventory: " + machine.getCount());
+			System.out.println("Current state: " + machine.getState()); 
+			
+		}
+	}
 
 }
