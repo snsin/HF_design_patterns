9091342cb45ab4769c9b4717c22e44077bd5b86b
smac <sin_s_n@mail.ru>
2016-05-16 00:07:11 +0300

    throws exception

--- DesignPatterns/src/exer/serg/state/GumballMachineTestDrive.java
+++ DesignPatterns/src/exer/serg/state/GumballMachineTestDrive.java
@@ -1,8 +1,10 @@
 package exer.serg.state;
 
+import java.rmi.RemoteException;
+
 public class GumballMachineTestDrive {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RemoteException {
 		GumballMachine gumballMachine = new GumballMachine(5);
 		System.out.println(gumballMachine);
 		gumballMachine.insertQuarter();
