bcae88771098cb15a4bcb4432e1503bf52fd45e0
smac <sin_s_n@mail.ru>
2016-05-17 00:40:06 +0300

    refactored. ImageProxy with state pattern

---
+++ DesignPatterns/src/exer/serg/proxy/ImageProxy.java
@@ -0,0 +1,37 @@
+package exer.serg.proxy;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.net.URL;
+
+import javax.swing.Icon;
+
+class ImageProxy implements Icon {
+	
+	private Icon currentState;
+	
+	public ImageProxy(URL url) {
+		currentState = new IconDownloadingState(this, url);
+	}
+	
+	@Override
+	public int getIconWidth() {
+		return currentState.getIconWidth();
+	}
+	
+	@Override
+	public int getIconHeight() {
+		return currentState.getIconHeight();
+	}
+	
+	@Override
+	public void paintIcon(final Component c, Graphics g, int x, int y) {
+		currentState.paintIcon(c, g, x, y);
+	}
+	
+	public void setState(Icon state) {
+		currentState = state;
+	}
+
+
+}
