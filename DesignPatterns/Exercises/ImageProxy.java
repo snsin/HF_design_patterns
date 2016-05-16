f70902515a69850e705abdbd0473a9f8ce55bbde
smac <sin_s_n@mail.ru>
2016-05-16 23:01:08 +0300

    created ImageProxy

---
+++ DesignPatterns/Exercises/ImageProxy.java
@@ -0,0 +1,59 @@
+import java.awt.Component;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+
+class ImageProxy implements Icon {
+	private ImageIcon imageIcon;
+	private URL imageURL;
+	private Thread retrievalThread;
+	private boolean retrieving = false;
+	
+	public ImageProxy(URL url) {
+		imageURL = url;
+	}
+	
+	@Override
+	public int getIconWidth() {
+		if (imageIcon != null) {
+			return imageIcon.getIconWidth();
+		} else {
+			return 800;
+		}
+	}
+	
+	@Override
+	public int getIconHeigth() {
+		if (imageIcon != null) {
+			return imageIcon.getIconHeight();
+		} else {
+			return 600;
+		}
+	}
+	
+	@Override
+	public void paintIcon(final Component c, Graphics g, int x, int y) {
+		if (imageIcon != null) {
+			imageIcon.paintIcon(c, g, x, y);
+		} else {
+			g.drawString("Loading CD cover, please wait...", x + 300, y + 190);
+			if (!retrieving) {
+				retrieving = true;
+				retrievalThread = new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						try {
+							imageIcon = new ImageIcon(imageURL, "CD Cover");
+							c.repaint();
+						} catch (Exception e) {
+							e.printStackTrace(s);							
+						}
+						
+					}
+				});
+				retrievalThread.start();
+			}
+		}
+	}
+}
