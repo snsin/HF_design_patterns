00d7ee2fdf4cee55ce00a07ce104730ffedc955b
smac <sin_s_n@mail.ru>
2016-05-17 14:45:56 +0300

    realized getOwnerProxy and getNonOwnerProxy methods

--- DesignPatterns/src/exer/serg/proxy/protection/MatchMakingTestDrive.java
+++ DesignPatterns/src/exer/serg/proxy/protection/MatchMakingTestDrive.java
@@ -1,5 +1,28 @@
 package exer.serg.proxy.protection;
 
+import java.lang.reflect.Proxy;
+
 public class MatchMakingTestDrive {
+	
+	public static void main(String[] args) {
+		new MatchMakingTestDrive().drive();
+	}
+
+	private void drive() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	PersonBean getOwnerProxy(PersonBean person) {
+		Class<? extends PersonBean> personClass = person.getClass();
+		return (PersonBean) Proxy.newProxyInstance(personClass.getClassLoader(), 
+				personClass.getInterfaces(), new OwnerInvocationHandler(person));
+	}
+	
+	PersonBean getNonOwnerProxy(PersonBean person) {
+		Class<? extends PersonBean> personClass = person.getClass();
+		return (PersonBean) Proxy.newProxyInstance(personClass.getClassLoader(), 
+				personClass.getInterfaces(), new NonOwnerInvocationHandler(person));
+	}
 
 }
