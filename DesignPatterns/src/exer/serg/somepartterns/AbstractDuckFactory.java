2877e7cfa4b63c93ff020d9eed6387a2214b32f7
smac <sin_s_n@mail.ru>
2016-05-23 13:24:15 +0300

    realized AbstractDuckFactory

---
+++ DesignPatterns/src/exer/serg/somepartterns/AbstractDuckFactory.java
@@ -0,0 +1,9 @@
+package exer.serg.somepartterns;
+
+public abstract class AbstractDuckFactory {
+	public abstract Quackable createMallardDuck();
+	public abstract Quackable createResheadDuck();
+	public abstract Quackable createDuckCall();
+	public abstract Quackable createRubberDuck();
+	
+}
