c724a878d0c03d06ed78b09033dbe58bf585820b
smac <sin_s_n@mail.ru>
2016-05-23 13:56:45 +0300

    changed

--- DesignPatterns/src/exer/serg/somepartterns/DuckSimulator.java
+++ DesignPatterns/src/exer/serg/somepartterns/DuckSimulator.java
@@ -4,17 +4,18 @@ public class DuckSimulator {
 
 	public static void main(String[] args) {
 		DuckSimulator simulator = new DuckSimulator();
-		simulator.simulate();
+		AbstractDuckFactory duckFactory = new CountingDuckFactory();
+		simulator.simulate(duckFactory);
 	}
 
-	private void simulate() {
-		Quackable mallardDuck = new QuackCounter(new MallardDuck());
-		Quackable redheadDuck = new QuackCounter(new RedheadDuck());
-		Quackable duckCall = new QuackCounter(new DuckCall());
-		Quackable rubberDuck = new QuackCounter(new RubberDuck());
+	private void simulate(AbstractDuckFactory duckFactory) {
+		Quackable mallardDuck = duckFactory.createMallardDuck();
+		Quackable redheadDuck = duckFactory.createRedheadDuck();
+		Quackable duckCall = duckFactory.createDuckCall();
+		Quackable rubberDuck = duckFactory.createRubberDuck();
 		Quackable gooseDuck = new GooseAdapter(new Goose());
 				
-		System.out.println("\nDuck Simulator");
+		System.out.println("\nDuck Simulator: With Abstract Factory");
 		
 		simulate(mallardDuck);
 		simulate(redheadDuck);
