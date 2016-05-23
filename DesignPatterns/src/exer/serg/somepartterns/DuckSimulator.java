ce74ea827348288b15c66ed548c1608742961af7
smac <sin_s_n@mail.ru>
2016-05-23 12:40:29 +0300

    changed DuckSimulator

--- DesignPatterns/src/exer/serg/somepartterns/DuckSimulator.java
+++ DesignPatterns/src/exer/serg/somepartterns/DuckSimulator.java
@@ -8,17 +8,21 @@ public class DuckSimulator {
 	}
 
 	private void simulate() {
-		Quackable mallardDuck = new MallardDuck();
-		Quackable redheadDuck = new RedheadDuck();
-		Quackable duckCall = new DuckCall();
-		Quackable rubberDuck = new RubberDuck();
-		
+		Quackable mallardDuck = new QuackCounter(new MallardDuck());
+		Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+		Quackable duckCall = new QuackCounter(new DuckCall());
+		Quackable rubberDuck = new QuackCounter(new RubberDuck());
+		Quackable gooseDuck = new GooseAdapter(new Goose());
+				
 		System.out.println("\nDuck Simulator");
 		
 		simulate(mallardDuck);
 		simulate(redheadDuck);
 		simulate(duckCall);
 		simulate(rubberDuck);
+		simulate(gooseDuck);
+		
+		System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times");
 	}
 
 	private void simulate(Quackable duck) {
