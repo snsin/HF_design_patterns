24010e7ad72437c49f8f33705617758e3afdd46f
smac <sin_s_n@mail.ru>
2016-05-23 13:29:25 +0300

    corrected misprint: createResheadDuck() is changed to createRedheadDuck()

--- DesignPatterns/src/exer/serg/somepartterns/AbstractDuckFactory.java
+++ DesignPatterns/src/exer/serg/somepartterns/AbstractDuckFactory.java
@@ -2,7 +2,7 @@ package exer.serg.somepartterns;
 
 public abstract class AbstractDuckFactory {
 	public abstract Quackable createMallardDuck();
-	public abstract Quackable createResheadDuck();
+	public abstract Quackable createRedheadDuck();
 	public abstract Quackable createDuckCall();
 	public abstract Quackable createRubberDuck();
 	
