4913e7328cf3a03047b5aaacbf142a4e3087459d
smac <sin_s_n@mail.ru>
2016-05-28 23:43:41 +0300

    added source code from HF_DP

--- DesignPatterns/src/exer/serg/mvc/BeatBar.java
+++ DesignPatterns/src/exer/serg/mvc/BeatBar.java
@@ -1,5 +1,32 @@
 package exer.serg.mvc;
 
-public class BeatBar {
+import javax.swing.JProgressBar;
+
+public class BeatBar extends JProgressBar implements Runnable {
+
+	private static final long serialVersionUID = 7695678551001316652L;
+	private Thread thread;
+
+	public BeatBar() {
+		thread = new Thread(this);
+		setMaximum(100);
+		thread.start();
+	}
+
+	@Override
+	public void run() {
+		for(;;) {
+			int value = getValue();
+			value = (int)(value * .75);
+			setValue(value);
+			repaint();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
 }
