3c0fec25088ab67a7519a324aa10c8e2d2775271
smac <sin_s_n@mail.ru>
2016-05-28 19:27:19 +0300

    realized Observable

---
+++ DesignPatterns/src/exer/serg/somepartterns/Observable.java
@@ -0,0 +1,27 @@
+package exer.serg.somepartterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Observable implements QuackObservable {
+	
+	private List<Observer> observers = new ArrayList<>();
+	private QuackObservable duck;
+	
+	public Observable(QuackObservable duck) {
+		this.duck = duck;
+	}
+	
+	@Override
+	public void registerObserver(Observer observer) {
+		observers.add(observer);
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Observer observer : observers) {
+			observer.update(duck);
+		}
+	}
+
+}
