aef9b8b9425df30203edbc76780f06685be2950e
smac <sin_s_n@mail.ru>
2016-05-28 19:37:35 +0300

    implemented QuackObservable

--- DesignPatterns/src/exer/serg/somepartterns/RubberDuck.java
+++ DesignPatterns/src/exer/serg/somepartterns/RubberDuck.java
@@ -2,9 +2,26 @@ package exer.serg.somepartterns;
 
 public class RubberDuck implements Quackable {
 
+	private Observable observable;
+	
+	public RubberDuck() {
+		observable = new Observable(this);
+	}
+	
 	@Override
 	public void quack() {
 		System.out.println("Squeak");
+		notifyObservers();
+	}
+
+	@Override
+	public void registerObserver(Observer observer) {
+		observable.registerObserver(observer);
+	}
+
+	@Override
+	public void notifyObservers() {
+		observable.notifyObservers();
 	}
 
 }
