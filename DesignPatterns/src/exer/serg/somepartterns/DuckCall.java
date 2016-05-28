a0977750750ef6cfc2ad086519d2c678109b231a
smac <sin_s_n@mail.ru>
2016-05-28 19:39:53 +0300

    implemented QuackObservable

--- DesignPatterns/src/exer/serg/somepartterns/DuckCall.java
+++ DesignPatterns/src/exer/serg/somepartterns/DuckCall.java
@@ -2,10 +2,26 @@ package exer.serg.somepartterns;
 
 public class DuckCall implements Quackable {
 
+	private Observable observable;
+	
+	public DuckCall() {
+		observable = new Observable(this);
+	}
+	
 	@Override
 	public void quack() {
 
 		System.out.println("Kwak");
+	}
+
+	@Override
+	public void registerObserver(Observer observer) {
+		observable.registerObserver(observer);
+	}
+
+	@Override
+	public void notifyObservers() {
+		observable.notifyObservers();
 	}
 
 }
