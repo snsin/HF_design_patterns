316b59d3b7b7e2c51c12dd7032c47fcaa881837f
smac <sin_s_n@mail.ru>
2016-05-28 23:08:04 +0300

    created DjView srub

---
+++ DesignPatterns/src/exer/serg/mvc/DjView.java
@@ -0,0 +1,44 @@
+package exer.serg.mvc;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DjView implements BpmObserver, BeatObserver, ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void createView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void createControls() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void disableStopMenuItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void enableStartMenuItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void disableStartMenuItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void enableStopMenuItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
