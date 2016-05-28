97982b3ae8dfa1bc41d7f7e506011dd149b04f16
smac <sin_s_n@mail.ru>
2016-05-28 22:24:20 +0300

    realized beat model interface

---
+++ DesignPatterns/src/exer/serg/mvc/BeatModelInterface.java
@@ -0,0 +1,19 @@
+package exer.serg.mvc;
+
+public interface BeatModelInterface {
+
+	void initialize();
+
+	void on();
+
+	void off();
+
+	void registerObserver(BeatObserver o);
+
+	void removeObserver(BeatObserver o);
+
+	void registerObserver(BpmObserver o);
+
+	void removeObserver(BpmObserver o);
+
+}
