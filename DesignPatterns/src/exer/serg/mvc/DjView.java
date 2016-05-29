e264cebe1973ef2ce5cefb8c31e8a1ded209d7f6
smac <sin_s_n@mail.ru>
2016-05-29 12:58:30 +0300

    optimized import

--- DesignPatterns/src/exer/serg/mvc/DjView.java
+++ DesignPatterns/src/exer/serg/mvc/DjView.java
@@ -3,11 +3,9 @@ package exer.serg.mvc;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.MenuBar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
