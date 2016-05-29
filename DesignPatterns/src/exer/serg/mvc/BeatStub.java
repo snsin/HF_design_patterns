68f07748b5a070d1403f93517878dfdee1b53cf7
smac <sin_s_n@mail.ru>
2016-05-29 14:14:58 +0300

    edited BeatStub

--- DesignPatterns/src/exer/serg/mvc/BeatStub.java
+++ DesignPatterns/src/exer/serg/mvc/BeatStub.java
@@ -4,15 +4,9 @@ import java.util.TimerTask;
 
 public class BeatStub extends TimerTask {
 	
-	private BeatModelInterface beatModel;
-
-	public BeatStub(BeatModelInterface beatModel) {
-		this.beatModel = beatModel;
-	}
-
 	@Override
 	public void run() {
-		beatModel.beatEvent();
+		System.out.println("beat event");
 
 	}
 
