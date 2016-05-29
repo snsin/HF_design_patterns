100a0550ea631f9852a3299a2fbeba3645730dd6
smac <sin_s_n@mail.ru>
2016-05-29 12:59:33 +0300

    realized DjTestDrive

--- DesignPatterns/src/exer/serg/mvc/DjTestDrive.java
+++ DesignPatterns/src/exer/serg/mvc/DjTestDrive.java
@@ -1,23 +1,8 @@
 package exer.serg.mvc;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class DjTestDrive {
 	public static void main(String...args) {
 		BeatModel model = new BeatModel();
-		final ControllerInterface controller = new BeatController(model);
-		controller.start();
-		Timer updBpm = new Timer();
-		updBpm.schedule(new TimerTask() {
-			
-			@Override
-			public void run() {
-				controller.increaseBpm();
-				
-			}
-		}, 10000, 5000);
+		final ControllerInterface controller = new BeatController(model);		
 	}
-	
-
 }
