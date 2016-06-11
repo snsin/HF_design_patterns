22e08d02217dadce345700955e49fa1e8511a1a2
smac <sin_s_n@mail.ru>
2016-06-11 21:17:02 +0300

    created for HeartController

--- DesignPatterns/src/exer/serg/mvc/DjTestDrive.java
+++ DesignPatterns/src/exer/serg/mvc/DjTestDrive.java
@@ -3,6 +3,11 @@ package exer.serg.mvc;
 public class DjTestDrive {
 	public static void main(String...args) {
 		HeartModel model = new HeartModel();
-		final ControllerInterface controller = new HeartController(model);		
+		final ControllerInterface controller = new HeartController(model);	
+		
+		for (String key : System.getenv().keySet()) {
+			System.out.println(key + " : " + System.getProperty(((String)key)));
+		}
+		Object name = new Object();
 	}
 }
